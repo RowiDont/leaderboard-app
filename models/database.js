5c41d9b78f802383107c2231d5a23842741a2707
Rafael <rafiepatel@gmail.com>
2016-02-28 16:18:42 -0500

    Add @ to connection string.

--- models/database.js
+++ models/database.js
@@ -1,7 +1,7 @@
 var pg = require('pg');
 var host = process.env.OPENSHIFT_POSTGRESQL_DB_HOST;
 var port = process.env.OPENSHIFT_POSTGRESQL_DB_PORT;
-var connectionString = "pg://" + host + ":" + port + "/leaderboard";
+var connectionString = "pg://adminunl69sf:am7dXX6vY_jy@" + host + ":" + port + "/leaderboard";
 // var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/leaderboard';
 
 var client = new pg.Client(connectionString);

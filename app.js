0ab272bb004368b50e4aff125dff9678eb81f0fa
Rafael <rafiepatel@gmail.com>
2016-02-28 18:33:49 -0500

    Move cors to app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
+var cors = require('cors');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,6 @@ var pg = require('pg');
 var host = process.env.OPENSHIFT_POSTGRESQL_DB_HOST;
 var port = process.env.OPENSHIFT_POSTGRESQL_DB_PORT;
 var connectionString = "pg://adminunl69sf:am7dXX6vY_jy@" + host + ":" + port + "/leaderboard";
-var cors = require('cors');
-
-
-express.use(cors());
 
 /* GET home page. */
 router.get('/api/scores', function(req, res) {
